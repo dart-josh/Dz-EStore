df97fe516317001c344a556369f913ee09d9b2bb
dart-josh <jboyadelooye@gmail.com>
2025-10-10 13:53:12 +0100

    Routes setup

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoute from "./routes/auth.route.js"
+import authRoute from "./routes/auth.route.js";
+import cartRoute from "./routes/cart.route.js";
+import managerRoute from "./routes/manager.route.js";
+import productRoute from "./routes/product.route.js";
+import storeRoute from "./routes/store.route.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -13,6 +18,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute)
+app.use("/api/cart", cartRoute)
+app.use("/api/manager", managerRoute)
+app.use("/api/product", productRoute)
+app.use("/api/store", storeRoute)
+app.use("/api/user", userRoute)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
