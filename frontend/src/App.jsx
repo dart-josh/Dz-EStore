dfc28b433fbf7344eae1aed724171afc652db219
dart-josh <jboyadelooye@gmail.com>
2025-10-17 11:37:35 +0100

    Frontend update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      
+      <div className="h-screen w-screen bg-mont-500"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/UV_Components/Buttons.jsx
@@ -0,0 +1,9 @@
+
+export const Buttons = () => {
+  return (
+    <button class="bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
+      Save changes
+    </button>
+  );
+};
+
